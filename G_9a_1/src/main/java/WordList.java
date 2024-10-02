335214ee4f068edf200951267da2a50481d9d739
sebastianpollak <spollak@student.tgm.ac.at>
2024-10-02 15:36:37 +0200

    WordList and WordPair added

--- G_9a_1/src/main/java/WordList.java
+++ G_9a_1/src/main/java/WordList.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordList {
 
+    private List<WordPair> wordlist;
 
+    public WordList() {
+        wordlist = new ArrayList<>();
+    }
+
+    public void addWordPair(WortEintrag entry) {
+        wordlist.add(entry);
+    }
+
+    public boolean removeWordPair(String word) {
+        return wordlist.removeIf(entry -> entry.getWord().equals(word));
+    }
+
+    public WordPair getWordPair(int i) {
+        if (wordlist.isEmpty()) {
+            System.out.println("Keine WortEinträge in der Liste.");
+        } else {
+            return wordlist.get(i)
+        }
+    }
+
+    public List<WordPair> getWordlist() {
+        return wordlist;
+    }
 
 }
--- G_9a_1/src/main/java/WordPair.java
+++ G_9a_1/src/main/java/WordPair.java
@@ -1,3 +1,26 @@
 public class WordPair {
 
+    private String word = "";
+    private String url = "";
+
+    public WortEintrag(String word, String url) throws IllegalArgumentException {
+
+        if(word.length() >= 1) {
+            this.word = word;
+        }
+        else{
+            throw new IllegalArgumentException("Wort zu kurz.");
+        }
+
+        this.url = url;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
 }
